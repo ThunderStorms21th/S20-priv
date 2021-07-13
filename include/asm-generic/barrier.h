d104187dd12e4127905c6eac6fac1a88551ad8aa
feravolt <fera.volt@gmail.com>
2021-07-14 00:56:50 +0300

    Powersaving shet
    [@xxmustafacooTR]: Modified for kernel 4.9
    [@ThunderStorms21th] : Modified for kernel 4.14.xxx and 4.19.xxx
    
    Signed-off-by: feravolt <fera.volt@gmail.com>
    
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- include/asm-generic/barrier.h
+++ include/asm-generic/barrier.h
@@ -18,6 +18,7 @@
 #ifndef __ASSEMBLY__
 
 #include <linux/compiler.h>
+#include <asm-generic/processor.h>
 
 #ifndef nop
 #define nop()	asm volatile ("nop")
@@ -238,7 +239,7 @@ do {									\
 		VAL = READ_ONCE(*__PTR);			\
 		if (cond_expr)					\
 			break;					\
-		cpu_relax();					\
+		cpu_read_relax();				\
 	}							\
 	VAL;							\
 })
