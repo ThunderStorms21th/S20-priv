b1671e14276f89f3aaba36fd28e5c2ce278824b0
ThunderStorms21th <pinakastorm@gmail.com>
2021-09-05 12:55:19 +0200

    ts-build: update build script for each one device
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- ts-build.sh
+++ ts-build.sh
@@ -41,9 +41,10 @@ LOG=compile_build.log
 # Kernel name
 KERNEL_NAME="LOCALVERSION=-ThunderStormS-v1.0-DUF9-OneUI3"
 DEFCONFIG1=exynos9830-z3sxxx_defconfig
-DEFCONFIG2=exynos9830-x1sxx_defconfig
-DEFCONFIG3=exynos9830-y2sxxx_defconfig
+DEFCONFIG2=exynos9830-x1slte_defconfig
+DEFCONFIG3=exynos9830-y2slte_defconfig
 DEFCONFIG4=exynos9830-x1sxxx_defconfig
+DEFCONFIG5=exynos9830-y2sxxx_defconfig
 DEFCONFIG_TS=ts_defconfig
 # -----------------------------
 
@@ -64,8 +65,12 @@ CLEAN_DTB()
 		echo "rm files in : "$(pwd)/arch/arm64/boot/dts/*.dtb""
 		rm $(pwd)/arch/arm64/boot/dts/exynos/*.dtb
 		rm $(pwd)/arch/arm64/boot/dts/exynos/*.dtb_dtout
+		rm $(pwd)/arch/arm64/boot/dts/exynos/*.dtb.dtout
+		rm $(pwd)/arch/arm64/boot/dts/exynos/*.dtb.reverse.dts
 		rm $(pwd)/arch/arm64/boot/dts/samsung/*.dtb
 		rm $(pwd)/arch/arm64/boot/dts/samsung/*.dtb_dtout
+		rm $(pwd)/arch/arm64/boot/dts/samsung/*.dtb.dtout
+		rm $(pwd)/arch/arm64/boot/dts/samsung/*.dtb.reverse.dts
 		rm $(pwd)/arch/arm64/boot/boot.img-dtb
 		rm $(pwd)/arch/arm64/boot/boot.img-kernel
 	fi
@@ -114,7 +119,7 @@ BUILD_KERNEL_980F()
       # DTB for Exynos 9830 - SM-G980F
       echo "***** Compiling Device Tree Blobs *****"
       $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-980F/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
-      $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-980F/dtbo.img dt.configs/x1sxx.cfg -d ${DTB_DIR}/samsung
+      $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-980F/dtbo.img dt.configs/x1slte.cfg -d ${DTB_DIR}/samsung
     else
       echo "return to Main menu' 'Kernel STUCK in BUILD!"
     fi
@@ -138,7 +143,7 @@ BUILD_KERNEL_985F()
       # DTB for Exynos 9830 - SM-G985F
       echo "***** Compiling Device Tree Blobs *****"
       $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-985F/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
-      $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-985F/dtbo.img dt.configs/y2sxxx.cfg -d ${DTB_DIR}/samsung
+      $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-985F/dtbo.img dt.configs/y2slte.cfg -d ${DTB_DIR}/samsung
     else
       echo "return to Main menu' 'Kernel STUCK in BUILD!"
     fi
@@ -168,6 +173,30 @@ BUILD_KERNEL_981B()
     fi
 }
 
+BUILD_KERNEL_986B()
+{
+    # Make .config
+	cp -f $(pwd)/arch/arm64/configs/$DEFCONFIG5 $(pwd)/arch/arm64/configs/tmp_defconfig
+	cat $(pwd)/arch/arm64/configs/$DEFCONFIG_TS >> $(pwd)/arch/arm64/configs/tmp_defconfig
+    # Compile kernels
+    echo "***** Compiling kernel *****"
+    [ ! -d "out" ] && mkdir out
+    [ ! -d "out/SM-986B" ] && mkdir out/SM-986B
+    # SM-G91BF
+    make -j$(nproc) -C $(pwd) $KERNEL_NAME tmp_defconfig
+    make -j$(nproc) -C $(pwd) $KERNEL_NAME
+    [ -e arch/arm64/boot/Image.gz ] && cp arch/arm64/boot/Image.gz $(pwd)/out/SM-986B/Image.gz
+    if [ -e arch/arm64/boot/Image ]; then
+      cp arch/arm64/boot/Image $(pwd)/out/SM-986B/Image
+      # DTB for Exynos 9830 - SM-G986B
+      echo "***** Compiling Device Tree Blobs *****"
+      $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-986B/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
+      $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-986B/dtbo.img dt.configs/y2sxxx.cfg -d ${DTB_DIR}/samsung
+    else
+      echo "return to Main menu' 'Kernel STUCK in BUILD!"
+    fi
+}
+
 BUILD_DTB()
 {
     # Compile DTB/DTBO images
@@ -176,14 +205,18 @@ BUILD_DTB()
     $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-988B/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
     $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-980F/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
     $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-985F/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
+    $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-981B/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
+    $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-986B/dtb.img dt.configs/exynos9830.cfg -d ${DTB_DIR}/exynos
     # DTBO for Exynos 9830 SM-988B
     $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-988B/dtbo.img dt.configs/z3sxxx.cfg -d ${DTB_DIR}/samsung
     # DTBO for Exynos 9830 SM-980F
-    $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-980F/dtbo.img dt.configs/x1sxx.cfg -d ${DTB_DIR}/samsung
+    $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-980F/dtbo.img dt.configs/x1slte.cfg -d ${DTB_DIR}/samsung
     # DTBO for Exynos 9830 SM-985F
-    $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-985F/dtbo.img dt.configs/y2sxxx.cfg -d ${DTB_DIR}/samsung
+    $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-985F/dtbo.img dt.configs/y2slte.cfg -d ${DTB_DIR}/samsung
     # DTBO for Exynos 9830 SM-981B
     $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-981B/dtbo.img dt.configs/x1sxxx.cfg -d ${DTB_DIR}/samsung
+    # DTBO for Exynos 9830 SM-986B
+    $(pwd)/tools/mkdtimg cfg_create $(pwd)/out/SM-986B/dtbo.img dt.configs/y2sxxx.cfg -d ${DTB_DIR}/samsung
 }
 
 BUILD_RAMDISK_988B()
@@ -283,6 +316,30 @@ BUILD_RAMDISK_981B()
     rm -rf $(pwd)/builds/temp
 }
 
+BUILD_RAMDISK_986B()
+{
+    # SM-G986B
+    echo ""
+    echo "Building Ramdisk for SM-986B"
+    mv $(pwd)/out/SM-986B/Image $(pwd)/out/SM-986B/boot.img-kernel
+    mv $(pwd)/out/SM-986B/dtb.img $(pwd)/out/SM-986B/boot.img-dtb
+    mkdir $(pwd)/builds/temp
+    cp -rf $(pwd)/builds/aik/. $(pwd)/builds/temp
+    cp -rf $(pwd)/builds/ramdisk/. $(pwd)/builds/temp
+    rm -f $(pwd)/builds/temp/split_img/boot.img-kernel
+    rm -f $(pwd)/builds/temp/split_img/boot.img-dtb
+    mv $(pwd)/out/SM-986B/boot.img-kernel $(pwd)/builds/temp/split_img/boot.img-kernel
+    mv $(pwd)/out/SM-986B/boot.img-dtb $(pwd)/builds/temp/split_img/boot.img-dtb
+    echo "Done"
+    cd $(pwd)/builds/temp
+    ./repackimg.sh
+    echo SEANDROIDENFORCE >> image-new.img
+    cd ..
+    cd ..
+    mv $(pwd)/builds/temp/image-new.img $(pwd)/builds/SM986B-boot.img
+    rm -rf $(pwd)/builds/temp
+}
+
 MAIN()
 {
 
@@ -293,12 +350,14 @@ MAIN()
 	START_TIME_SEC=`date +%s`
         BUILD_KERNEL_988B
         BUILD_RAMDISK_988B
-        #BUILD_KERNEL_980F
-        #BUILD_RAMDISK_980F
         BUILD_KERNEL_981B
         BUILD_RAMDISK_981B
-        #BUILD_KERNEL_985F
-        #BUILD_RAMDISK_985F
+        BUILD_KERNEL_986B
+        BUILD_RAMDISK_986B
+        BUILD_KERNEL_980F
+        BUILD_RAMDISK_980F
+        BUILD_KERNEL_985F
+        BUILD_RAMDISK_985F
         # BUILD_DTB
     END_TIME=`date +%T`
     END_TIME_SEC=`date +%s`
