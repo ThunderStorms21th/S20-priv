2568446ca52a390d3033f78ed7a4620c9095aa00
ThunderStorms21th <pinakastorm@gmail.com>
2021-11-05 11:16:41 +0100

    scsi:ufs Async suspend/resume callbacks. The UFS callback is the most time consuming callback in the dpm_resume section of kernel resumes, taking around 30 ms. Making it async improves resume latency by around 20 ms, and helps with decreasing suspend times as well.
    Bug: 134704391
    Change-Id: I708c8a7bc8f2250d6b2365971ccc394c7fbf8896
    Signed-off-by: Vincent Palomares <paillon@google.com>
    
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -11028,6 +11028,8 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 	ufs_sysfs_add_nodes(hba->dev);
 	ufshcd_add_sysfs_nodes(hba);
 
+	device_enable_async_suspend(dev);
+
 	return 0;
 
 exit_gating:
