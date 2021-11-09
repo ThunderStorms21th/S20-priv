2821f448b59fd20b90a4c7092d27462f5e0bca96
ThunderStorms21th <pinakastorm@gmail.com>
2021-11-09 14:44:10 +0100

    Revert "ems: Energy_step_S21 - backport continue"
    This reverts commit 4cb7c5dcc72a0200bb261cba2d90aff53e68b192.

--- include/trace/events/ems_debug_S21.h
+++ include/trace/events/ems_debug_S21.h
@@ -7,10 +7,10 @@
  */
 
 #undef TRACE_SYSTEM
-#define TRACE_SYSTEM ems_debug_S21
+#define TRACE_SYSTEM ems_debug
 
-#if !defined(_TRACE_EMS_DEBUG_S21_H) || defined(TRACE_HEADER_MULTI_READ)
-#define _TRACE_EMS_DEBUG_S21_H
+#if !defined(_TRACE_EMS_DEBUG_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_EMS_DEBUG_H
 
 #include <linux/tracepoint.h>
 
