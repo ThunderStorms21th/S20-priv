5ef02f3efafcbb65b63e359d96cb0891d9323bb4
ThunderStorms21th <pinakastorm@gmail.com>
2021-12-09 19:43:07 +0100

    drivers/gpu: disable auto calibration DVFS freq for GPU
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- drivers/gpu/arm/v_r25p1/platform/exynos/mali_kbase_platform.c
+++ drivers/gpu/arm/v_r25p1/platform/exynos/mali_kbase_platform.c
@@ -294,7 +294,7 @@ static int gpu_dvfs_update_config_data_from_dt(struct kbase_device *kbdev)
 	platform->governor_type_init = platform->governor_type;
 #endif
 
-#if IS_ENABLED(CONFIG_CAL_IF)
+#if 0
 	platform->gpu_dvfs_start_clock = cal_dfs_get_boot_freq(platform->g3d_cmu_cal_id);
 	GPU_LOG(DVFS_INFO, DUMMY, 0u, 0u, "get g3d start clock from ect : %d\n", platform->gpu_dvfs_start_clock);
 #else
@@ -309,7 +309,7 @@ static int gpu_dvfs_update_config_data_from_dt(struct kbase_device *kbdev)
 
 	gpu_update_config_data_int(np, "gpu_pmqos_cpu_cluster_num", &platform->gpu_pmqos_cpu_cluster_num);
 	gpu_update_config_data_int(np, "gpu_max_clock", &platform->gpu_max_clock);
-#if IS_ENABLED(CONFIG_CAL_IF)
+#if 0
 	platform->gpu_max_clock_limit = (int)cal_dfs_get_max_freq(platform->g3d_cmu_cal_id);
 #else
 	gpu_update_config_data_int(np, "gpu_max_clock_limit", &platform->gpu_max_clock_limit);
@@ -595,7 +595,7 @@ static int gpu_context_init(struct kbase_device *kbdev)
 #endif
 
 #ifdef CONFIG_MALI_ASV_CALIBRATION_SUPPORT
-	platform->gpu_auto_cali_status = false;
+	// platform->gpu_auto_cali_status = false;
 #endif
 
 #ifdef CONFIG_MALI_SEC_CL_BOOST
