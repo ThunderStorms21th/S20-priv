006ea4653e85328988939cc22238b038b10a47e9
ThunderStorms21th <pinakastorm@gmail.com>
2022-02-23 08:43:20 +0100

    mali: r36p0 - added Samsung changes into arm drivers
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- drivers/gpu/arm/bv_r36p0/mali_kbase_core_linux.c
+++ drivers/gpu/arm/bv_r36p0/mali_kbase_core_linux.c
@@ -2106,6 +2106,9 @@ static long kbase_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		break;
 	}
 
+	if (!mali_exynos_ioctl(kctx, cmd, arg))
+		return 0;
+
 	dev_warn(kbdev->dev, "Unknown ioctl 0x%x nr:%d", cmd, _IOC_NR(cmd));
 
 	return -ENOIOCTLCMD;
@@ -4249,6 +4252,8 @@ static int kbase_common_reg_map(struct kbase_device *kbdev)
 		goto out_ioremap;
 	}
 
+	mali_exynos_coherency_reg_map();
+
 	return err;
 
 out_ioremap:
@@ -4266,6 +4271,8 @@ static void kbase_common_reg_unmap(struct kbase_device * const kbdev)
 		kbdev->reg_start = 0;
 		kbdev->reg_size = 0;
 	}
+
+	mali_exynos_coherency_reg_unmap();
 }
 #endif /* CONFIG_MALI_NO_MALI */
 
@@ -5249,6 +5256,8 @@ int kbase_sysfs_init(struct kbase_device *kbdev)
 			&kbase_attr_group);
 	}
 
+	mali_exynos_sysfs_set_gpu_model_callback(&gpuinfo_show);
+
 	return err;
 }
 
@@ -5388,6 +5397,9 @@ static int kbase_device_resume(struct device *dev)
 	if (!kbdev)
 		return -ENODEV;
 
+	/* MALI_SEC_INTEGRATION */
+	mali_exynos_set_pm_state_resume_begin();
+
 	kbase_pm_resume(kbdev);
 
 #ifdef CONFIG_MALI_MIDGARD_DVFS
@@ -5404,6 +5416,10 @@ static int kbase_device_resume(struct device *dev)
 		flush_workqueue(kbdev->devfreq_queue.workq);
 	}
 #endif
+
+	/* MALI_SEC_INTEGRATION */
+	mali_exynos_set_pm_state_resume_end();
+
 	return 0;
 }
 
@@ -5520,7 +5536,9 @@ static int kbase_device_runtime_idle(struct device *dev)
 	 * the autosuspend delay and so won't suspend the device immediately.
 	 */
 	pm_runtime_mark_last_busy(kbdev->dev);
-	return 0;
+	/* MALI_SEC_INTEGRATION */
+	/* Runtime IDLE must be return 1 for turn on next time by RuntimePM API!! */
+	return 1;
 }
 #endif /* KBASE_PM_RUNTIME */
 
@@ -5538,6 +5556,8 @@ static const struct dev_pm_ops kbase_pm_ops = {
 
 #if IS_ENABLED(CONFIG_OF)
 static const struct of_device_id kbase_dt_ids[] = {
+	/* MALI_SEC_INTEGRATION */
+	{ .compatible = "arm,mali", },
 	{ .compatible = "arm,malit6xx" },
 	{ .compatible = "arm,mali-midgard" },
 	{ .compatible = "arm,mali-bifrost" },
