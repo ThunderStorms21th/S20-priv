5eafa0c15de599d8d8123afb4ad56db95170a114
ThunderStorms21th <pinakastorm@gmail.com>
2022-04-22 07:51:12 +0200

    drivers/cpufreq: exynoos-ufc - change SCALE_SIZE from 8 to 2
    Signed-off-by: ThunderStorms21th <pinakastorm@gmail.com>

--- drivers/cpufreq/exynos-ufc.c
+++ drivers/cpufreq/exynos-ufc.c
@@ -9,7 +9,12 @@
  * published by the Free Software Foundation.
  */
 #define pr_fmt(fmt)	KBUILD_MODNAME ": " fmt
-#define SCALE_SIZE 8
+
+/*
+ * Log2 of the number of scale size. The frequencies are scaled up or
+ * down as the multiple of this number. Default is 8.
+ */
+#define SCALE_SIZE 2
 
 #define AUTO_FILL 1
 #define NOT_FILL 0
